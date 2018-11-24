3ec96ce565931b908de6f5d0cfec0f6c10b7b05a
Jenny ZHANG <jennyzsh@connect.hku.hk>
2018-11-25 01:20:01 +0800

    update

--- NewsApp/Classes/ViewControllers/BaseViewController.swift
+++ NewsApp/Classes/ViewControllers/BaseViewController.swift
@@ -18,12 +18,11 @@ class BaseViewController: UIViewController {
     
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
-
     }
     
     override func viewDidAppear(_ animated: Bool) {
         super.viewDidAppear(animated)
-//        self.resetContent()
+        self.resetContent()
     }
     
     @objc func resetContent() {
