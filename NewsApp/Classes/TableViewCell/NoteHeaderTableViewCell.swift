4f294842e222c3354ec86794ad0ab7f0c2568298
Jenny ZHANG <jennyzsh@connect.hku.hk>
2018-11-29 16:09:01 +0800

    add expandable table view cell

---
+++ NewsApp/Classes/TableViewCell/NoteHeaderTableViewCell.swift
@@ -0,0 +1,18 @@
+//
+//  NoteHeaderTableViewCell.swift
+//  NewsApp
+//
+//  Created by Jenny ZHANG on 2018/11/29.
+//  Copyright © 2018年 Jenny ZHANG. All rights reserved.
+//
+
+import UIKit
+
+class NoteHeaderTableViewCell: UITableViewCell {
+
+    @IBOutlet weak var lblTitle: UILabel!
+    override func awakeFromNib() {
+        super.awakeFromNib()
+    }
+
+}
--- NewsApp/Classes/ViewControllers/Note/NoteViewController.swift
+++ NewsApp/Classes/ViewControllers/Note/NoteViewController.swift
@@ -8,10 +8,17 @@
 
 import UIKit
 
-class NoteViewController: BaseViewController {
+class NoteViewController: BaseViewController, UITableViewDataSource, UITableViewDelegate {
 
+    @IBOutlet weak var tableView: UITableView!
+    
+    let headerCellIdentifier = "NoteHeaderTableViewCell"
+    var tableViewDataIndicator = [false, false, false]
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        self.tableView.register(UINib(nibName: headerCellIdentifier, bundle: nil), forCellReuseIdentifier: headerCellIdentifier)
 
     }
     
@@ -30,4 +37,38 @@ class NoteViewController: BaseViewController {
         self.setNavigationBarTitleView(lblTitle)
 
     }
+    
+    
+    //MARK: UITableViewDataSource
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 3
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        if tableViewDataIndicator[section] {
+            return 3
+        } else {
+            return 1
+        }
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        if indexPath.row == 0 {
+            let cell = tableView.dequeueReusableCell(withIdentifier: headerCellIdentifier, for: indexPath) as! NoteHeaderTableViewCell
+            cell.lblTitle.text = "Title"
+            return cell
+        } else {
+            let cell = UITableViewCell()
+            cell.textLabel?.text = "Cell"
+            return cell
+        }
+    }
+    
+    //MARK: UITableViewDelegate
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        tableViewDataIndicator[indexPath.section] = !tableViewDataIndicator[indexPath.section]
+        let sections = IndexSet.init(integer: indexPath.section)
+        tableView.reloadSections(sections, with: .none)
+    }
+    
 }
